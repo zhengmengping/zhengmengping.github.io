a9e1caaa39bad0f1a7f5db12350cb798e9bc7827
zhengmengping <18767157330@163.com>
2018-06-06 17:55:57 +0800

    Site updated: 2018-06-06 17:55:56

--- photos/photo.js
+++ photos/photo.js
@@ -21,7 +21,7 @@ photo ={
            imageY = imageSize.split('.')[1];
             li += '<div class="card" style="width:330px">' +
                     '<div class="ImageInCard" style="height:'+ 330 * imageY / imageX + 'px">' +
-                      '<a data-fancybox="gallery" href="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
+                      '<a data-fancybox="gallery" href="https://github.com/zhengmengping/blogPhotos/tree/master/Images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
                         '<img src="https://github.com/zhengmengping/blogPhotos/tree/master/Images/' + imgNameWithPattern + '?raw=true"/>' +
                       '</a>' +
                     '</div>' +
